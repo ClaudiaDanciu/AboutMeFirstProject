e0fdcc88b99a36ff8c39e95e30c26733ac406b1b
ClaudiaDanciu <contact@claudiadanciu.com>
2018-05-12 12:24:37 +0100

    resume
    resume

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Claudia Danciu | Resume</title>
+    <link href="https://fonts.googleapis.com/css?family=Caveat|Open+Sans:400,700" rel="stylesheet">
+    
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <!-- // Intro -->
+    <header>
+        <div class="content-wrap">
+        <img class ="profile-img col-narrow" src="claudiaD.png" alt="Claudia Danciu" >
+        <div class ="col-wide">
+            <h1>Claudia Danciu</h1>
+            <h2>Software Engineer</h2>
+            <p>As a developer, I specialise in creating modular and scalable front-end architectures.
+            I am a confident, highly motivated individual with a keen interest in programming. 
+            Very enthusiastic, driven by giving a truthful customer service and experience,
+            with a particular-skill when it comes to consolidating the areas that work well
+            and tackling the challenges that slow down growth in a business so that the 
+            organisation can maximise its full potential.</p>
+        </div>
+        </div>
+    </header>
+    
+    <main> 
+        <!-- // Work Experience -->
+        <!--do this work experience like an accordeon-->
+        
+        <section class="work">
+           <div class="content-wrap">
+               
+            <h2>Work Experience</h2>
+
+            <!-- Job Details:  -->
+            <!--Job1-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Software Engineering Trainee</h3>
+                <p>Sky</p>
+                <p>Jan 2018 - Present</p>
+            </div>
+            
+            <div class ="col-wide">
+                <p>Awareness of Technology and current trends in software development.
+                Agile delivery. Software development – PHP; JavaScript; HTML & CSS.
+                Object Oriented Programming. Databases; MySQL. Software build and integration.
+                Case study and available online resources.</p>
+             
+            </div>
+            </div>
+           
+            <!--Job2-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>General Manager - Project Based</h3>
+                <p>Dentivo</p>
+                <p>Nov 2015 - Oct 2017</p>
+            </div>
+           
+            <div class ="col-wide">
+                <p>Directing various business departments to ensure the smooth running of
+                the company, resolving contractual and commercial issues and disputes.
+                Developing positive direct relationships with key business contacts,
+                scheduling workload to meet priorities and targets and using automated
+                computer systems to monitor stock control. Maintaining excellent 
+                relationships with clients and suppliers and decides budgets for all department,
+                initialize contract negotiation. Promote the company digital and create campaigns.
+                Developed marketing programs for a variety of business-to-business clients.
+                Used an integrated approach to create balanced programs for clients.</p>
+            </div>    
+            </div>
+        
+          
+            <!--Job3-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Banking Officer</h3>
+                <p>Transilvania Bank</p>
+                <p>Jul 2014 - Feb 2017</p>
+            </div>
+            
+            <div class ="col-wide">
+                <p>Managed and negotiated current/new accounts, developed and administered new
+                operational processes. Organized and processed account receivables and monitored
+                computer applications.Developed communication skills that helped to realize 
+                customer retention while making operations, marketed specific products and 
+                services by targeting needs of current and potential clients. 
+                Cross-Selling/Up-Selling to customers, dealing with both private and corporate
+                customers, while sometimes working in regional or head offices. Ensuring 
+                there’s a high level of customer service. Keeping accurate records of all
+                daily transactions. Advising corporate clients about mergers, acquisitions,
+                capital markets etc. Training and supervising junior banking staff. Understanding
+                of banking policies, regulations, and procedures. </p>
+            </div>
+            </div>
+            <!--Job4-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Banking Officer</h3>
+                <p>BCR (Erste Group) Bank</p>
+                <p>Jul 2013 - Mar 2014</p>
+            </div>
+            
+            <div class ="col-wide">
+            <p>Supported and assisted client’s day-to-day banking needs by giving them
+                an accurate, efficient, and professional service and providing assistance
+                after the sale of the product. Understood clients financial needs and 
+                performed a quick analysis to recommend the best solution for their 
+                requirements. Gave clients precise initial information regarding mortgage
+                rates, loans, and investments. Supported Financial Advisors when dealing
+                with Spanish and French speaking clients by acting as an intermediary.</p>
+            </div>
+            </div>
+            <!-- Job Details: -->
+            <!--Job5-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Intern</h3>
+                <p>BRD - Groupe Societe Generale</p>
+                <p>Feb 2013 - May 2013</p>
+            </div>
+            
+            <div class ="col-wide">
+                <p>Collaborated with specialists in banking and acquaintance with a branch
+                structure. Gained knowledge about Bank documents and workflow being 
+                involved in various activities. Helped with post sales support for clients.</p>
+            </div> 
+            </div>
+
+            <!-- Job Details:  -->
+            <!--Job6-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Call Center Operator</h3>
+                <p>CityAudit</p>
+                <p>Jul 2011 - Oct 2011</p>
+            </div>
+           
+            <div class ="col-wide">
+                <p>Processed information in a fast paced environment, dealt with calls in a highly
+                professional manner. Solved complaints in a diplomatic way, accurately and 
+                updated customer records in real time. Responded to various types of customers,
+                including ones with special communication needs, such as language difficulties
+                or disabilities while adapting to the needs of all customers, being fully aware
+                of all laws and regulation regarding data protection.</p>
+            </div>
+            </div>
+            <!--Job7-->
+            <div id="job">
+            <div class ="col-narrow">
+                <h3>Hotel Operations Supervisor</h3>
+                <p>Alma Mater Complex</p>
+                <p>Apr 2011 - Jul 2011</p>
+            </div>
+           
+            <div class ="col-wide">
+                <p>Responsible for providing a friendly welcome and a high-quality service over 
+                the phone to guests arriving at the hotel. Constantly portraying a highly 
+                professional image of the hotel and paying particular attention to guest 
+                satisfaction and efficiency. Dealing with bookings by phone, e-mail, letter,
+                fax or face-to-face and dealing with complaints or problems </p>
+            </div>
+            </div>
+            <!--Job8-->
+            <div id="job">
+                <div class ="col-narrow">
+                <h3>Sales Representative</h3>
+                <p>A&amp;H Advertising</p>
+                <p>Mar 2007 - Sep 2009</p>
+            </div>
+            
+            <div class ="col-wide">
+            <p>Established new accounts by planning and organizing daily work schedule
+                to call on existing or potential sales outlets. Supported sales efforts 
+                by studying existing and potential volume of dealers. Recommended changes
+                in products, services, and policies by evaluating results and competitive
+                developments. Resolved customer complaints by investigating problems, 
+                developing solutions, preparing reports and making recommendations to management.</p>
+            </div>
+            </div>
+            <!--Job9-->
+            <div id="job">
+                <div class ="col-narrow">
+                <h3>Media Coordinator Department</h3>
+                <p>LSTGM</p>
+                <p>Oct 2007 - May 2009</p>
+            </div>
+            
+            <div class ="col-wide">
+                <p>Planned and scheduled programming and events coverage, based on broadcast length,
+                time availability, and other factors, such as community needs, rating data,
+                and viewer demographics.Monitored and reviewed programming to ensure that 
+                schedules are met, guidelines are adhered to, and performances are of adequate quality. 
+                Coordinated activities between departments and developed ideas for programs and new activities.
+            </div>
+            </div>
+                <!--Job10-->
+            <div id= "job">
+            <div class ="col-narrow">
+                <h3>Financial Consultant Intern</h3>
+                <p>ING</p>
+                <p>Jan 2007 - Sep 2007</p>
+            </div>
+           
+            <div class ="col-wide">
+                <p>Responsible for providing a friendly welcome and a high-quality service over the
+                phone to guests arriving at the hotel. Constantly portraying a highly professional
+                image of the hotel and paying particular attention to guest satisfaction and efficiency.
+                Dealing with bookings by phone, e-mail, letter, fax or face-to-face and dealing with 
+                complaints or problems 
+            </div>    
+            </div>   
+                
+        
+                <!-- end of Job Details block. -->
+       </div>
+
+       </section>
+
+
+        <!-- // Education -->
+        <section class="education">
+            <div class="content-wrap">
+            <h2>Education</h2>
+
+            <!-- School details: -->
+            <h3>Babes Bolyai University, 2014</h3>
+            <p>Bachelor's Degree, Finance and Banking</p>
+            <p>Thesis - Classical and Alternatives Financing Methods.</p>
+            <!-- End of school details. -->
+            </div>
+        </section>
+    </main>
+    
+    <!-- // Footer & contact info -->
+    <footer>
+      <div class="content-wrap">
+        <h2>Let's Keep in Touch!</h2>
+        
+        <!-- Social media and contact links. -->
+      <div class="contact-info">
+            <a href="mailto:contact@claudiadanciu.com">contact@claudiadanciu.com</a>
+            <a href="http://claudiadanciu.com" target="_blank">claudiadanciu.com</a>
+            <a href="#" target="_blank">Twitter</a>
+            <a href="https://www.linkedin.com/in/claudiadanciu/" target="_blank">LinkedIn</a>
+            <a href="#" target="_blank">Facebook</a>
+      </div>
+
+        <p>Copyright 2017 by &copy;Claudia Danciu</p>
+      </div>
+    </footer>
+</body>
+</html>
